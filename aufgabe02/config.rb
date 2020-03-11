4b6a3516cd539093fe29dec142f4b3aa1db4badc
Lea Exenberger <1910653633@stud.fh-kufstein.ac.at>
2020-03-11 20:10:19 +0100

    Die bearbeitete version

--- aufgabe02/config.rb
+++ aufgabe02/config.rb
@@ -7,10 +7,11 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :nested;
+
+
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
